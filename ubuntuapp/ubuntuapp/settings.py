076e1fbbe3e89111531f9698a94a691bfb7a7dab
PowerHF India Pvt Ltd. <158015043+Powerhf@users.noreply.github.com>
2025-01-13 17:30:29 +0530

    Update settings.py

--- ubuntuapp/ubuntuapp/settings.py
+++ ubuntuapp/ubuntuapp/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.pathjoin(BASE_DIR, 'static)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
